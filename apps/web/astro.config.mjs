b9035d734c7b0d51afe7ab449b29d5019f030d81
alikia2x <alikia2x@outlook.com>
2026-03-25 21:00:29 +0800

    feat(web): scaffold the song info page & i18n

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,8 +2,24 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 import react from "@astrojs/react";
+import node from "@astrojs/node";
+import ssrCopyPlugin from "@lib/copy-plugin";
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [UnoCSS({ injectReset: true }), react()],
+	adapter: node({
+		mode: "standalone",
+	}),
+	vite: {
+		plugins: [
+			ssrCopyPlugin({
+				src: "../../locale",
+				dest: "locale",
+			}),
+		],
+	},
+	devToolbar: {
+		enabled: false,
+	},
+	output: "server",
 });
